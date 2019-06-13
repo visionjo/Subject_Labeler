67f723fc2eb908a836681742d4bb04b1a0455453
Joe Robinson <robinson.jo@husky.neu.edu>
2019-06-13 11:01:47 -0400

    first commit

---
+++ src/cluster_viewer/Cluster_Viewer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluster_viewer;
+import views.main.Main_Frame;
+import java.io.IOException;
+/**
+ *
+ * @author jrobby
+ */
+
+
+public class Cluster_Viewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        Main_Frame mf = new Main_Frame();
+        mf.setVisible(true);
+    }
+    
+}
