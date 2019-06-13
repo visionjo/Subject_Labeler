b28ec83a30e53d1718628328e6e80ec093d03d69
Joe Robinson <robinson.jo@husky.neu.edu>
2019-06-13 13:43:39 -0400

    added class for face samples

---
+++ src/components/FaceImage.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package components;
+
+import javax.swing.JButton;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.TitledBorder;
+import views.main.Main_Frame;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Vector;
+
+import components.HSLColor;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.imageio.ImageIO;
+import views.Sample;
+
+/**
+ * Image with button functionality
+ *
+ * @author josephrobinson
+ */
+public class FaceImage extends JButton {
+
+    // this value determines the range of colors that span for the border of the
+    // image-- based on hue angle, red is 0 and 180 is green (0=red, 1=green)    
+    static final float HUE_ULIMIT = 180.0f;
+    static final int BORDER_THICKNESS = 3;
+    static final int IMWIDTH = 75;
+    static final int IMHEIGHT = 75;
+    Image imface;
+
+    // sample name
+    String impath;
+
+    // the cluster in its fid
+    int cluster_id;
+
+    // the cluster in its fid
+    float confidence;
+
+    boolean isloaded = false;
+    HSLColor border_color;
+    // Borders
+    TitledBorder noFocus = BorderFactory.createTitledBorder(
+            BorderFactory.createEmptyBorder());
+
+    TitledBorder focused;
+
+    FaceImage(String path, int cluster, float score, String ref) {
+        this.impath = path;
+        this.cluster_id = cluster;
+        this.confidence = score;
+
+        this.loadImage();
+        this.initComponents();
+
+    }
+
+    private void initComponents() {
+
+        // set color to paint border
+        float hue = this.HUE_ULIMIT * this.confidence;
+        this.border_color = new HSLColor(hue);
+        // create and add border to image icon to represent confidence
+        this.focused = BorderFactory.createTitledBorder(
+                BorderFactory.createLineBorder(border_color.getRGB(), this.BORDER_THICKNESS));
+
+        this.setIcon(new ImageIcon(this.imface));
+
+        // Set the border with the name of the sample
+        this.setBorder(this.noFocus);
+
+        // removes the "button" look
+        this.setContentAreaFilled(false);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.out.println("clicked");
+            }
+        });
+
+        this.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // add border
+                FaceImage.this.setBorder(FaceImage.this.focused);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // remove border
+                FaceImage.this.setBorder(FaceImage.this.noFocus);
+            }
+
+        });
+    }
+
+
+    private void scaleImage(){
+    /**
+     * Resizes an image using a Graphics2D object backed by a BufferedImage.
+     */
+    if(!this.isloaded)
+        return;
+   
+    BufferedImage resizedImg = new BufferedImage(this.IMWIDTH, this.IMHEIGHT, 
+            BufferedImage.TYPE_INT_RGB);
+    Graphics2D g2 = resizedImg.createGraphics();
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, 
+           RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g2.drawImage(this.imface, 0, 0, this.IMWIDTH, this.IMHEIGHT, null);
+    g2.dispose();
+    this.imface = resizedImg;
+    }
+    private final void loadImage() {
+        /**
+         * Load image and rescale to proper size
+         */
+        BufferedImage img = null;
+        Sample samp;
+            try {
+                // read the image
+                img = ImageIO.read(new File(this.impath));
+                this.isloaded = true;
+            } 
+            catch (FileNotFoundException e) {
+                 System.out.println(e.getMessage());
+                this.isloaded = false;
+            } 
+            catch (IOException e) {
+                 System.out.println(e.getMessage());
+                this.isloaded = false;
+            }
+            this.scaleImage();
+        }
+  
+}
